e39b6c55c907a73c96aca29504ce46eab3ae1e8d
Naveen reddy tippasani <162026696+naveenchamp@users.noreply.github.com>
2025-10-02 17:44:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,70 +2,61 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const server = require('http').Server(app)
-
-// --- FIX #1: Added CORS configuration for deployment ---
-const io = require('socket.io')(server, {
-  cors: {
-    origin: "*", // Allows connections from any origin
-    methods: ["GET", "POST"]
-  }
-})
+const io = require('socket.io')(server)
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
 
-// Reminder: This in-memory object will be cleared if the server restarts.
 const rooms = { }
 
 app.get('/', (req, res) => {
-  res.render('index', { rooms: rooms })
+  res.render('index', { rooms: rooms })
 })
 
 app.post('/room', (req, res) => {
-  if (rooms[req.body.room] != null) {
-    return res.redirect('/')
-  }
-  rooms[req.body.room] = { users: {} }
-  res.redirect(req.body.room)
-  // Send message that new room was created
-  io.emit('room-created', req.body.room)
+  if (rooms[req.body.room] != null) {
+    return res.redirect('/')
+  }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  // Send message that new room was created
+  io.emit('room-created', req.body.room)
 })
 
 app.get('/:room', (req, res) => {
-  if (rooms[req.params.room] == null) {
-    return res.redirect('/')
-  }
-  res.render('room', { roomName: req.params.room })
+  if (rooms[req.params.room] == null) {
+    return res.redirect('/')
+  }
+  res.render('room', { roomName: req.params.room })
 })
 
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`)
+  console.log(`Server running at http://localhost:${PORT}`)
 })
 
 io.on('connection', socket => {
-  socket.on('new-user', (room, name) => {
-    socket.join(room)
-    rooms[room].users[socket.id] = name
-    socket.to(room).broadcast.emit('user-connected', name)
-  })
-  socket.on('send-chat-message', (room, message) => {
-    // --- FIX #2: Changed to io.to().emit() so the sender also receives the message ---
-    io.to(room).emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
-  })
-  socket.on('disconnect', () => {
-    getUserRooms(socket).forEach(room => {
-      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
-      delete rooms[room].users[socket.id]
-    })
-  })
+  socket.on('new-user', (room, name) => {
+    socket.join(room)
+    rooms[room].users[socket.id] = name
+    socket.to(room).broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', (room, message) => {
+    socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    getUserRooms(socket).forEach(room => {
+      socket.to(room).broadcast.emit('user-disconnected', rooms[room].users[socket.id])
+      delete rooms[room].users[socket.id]
+    })
+  })
 })
 
 function getUserRooms(socket) {
-  return Object.entries(rooms).reduce((names, [name, room]) => {
-    if (room.users[socket.id] != null) names.push(name)
-    return names
-  }, [])
+  return Object.entries(rooms).reduce((names, [name, room]) => {
+    if (room.users[socket.id] != null) names.push(name)
+    return names
+  }, [])
 }
